7ff6a6fe7a73807a1eda68f8822131e9ebc38434
Juan Correa <juan.correa.herrera@gmail.com>
2023-12-29 20:38:07 -0600

    feat: add daisyui package and update tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className="p-10">
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,6 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
