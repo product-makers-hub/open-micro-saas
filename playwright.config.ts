e5eedcd72f88dba89aff84b9ae2775e8f4a03a7b
Juan Correa <juan.correa.herrera@gmail.com>
2024-01-08 15:25:18 -0600

    feat: add NEXTAUTH_SECRET environment variable and update webServer command

--- playwright.config.ts
+++ playwright.config.ts
@@ -83,7 +83,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run build && npm run start",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
