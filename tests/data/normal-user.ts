5a884435a96a437444cf15f95c7ea245e3a44cdf
Juan Correa <juan.correa.herrera@gmail.com>
2024-01-08 20:53:55 -0600

    feat: add normal user login test

---
+++ tests/data/normal-user.ts
@@ -0,0 +1,8 @@
+export const normalUser = {
+  email: "user@open-saas.com",
+  name: "Jimmy Doe",
+  plainPassword: "user",
+  appUrl: "/",
+  loginUrl: "/api/auth/signin",
+  storageSessionPath: "./tests/data/normal-user.session.json",
+};
--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -1,16 +1,22 @@
 import { test as setup, expect } from "@playwright/test";
 
 import { adminUser } from "./data/admin-user";
+import { normalUser } from "./data/normal-user";
 import { truncateDb } from "../prisma/truncate-db";
-import { createAdminRoleAndUser } from "../prisma/seeds/seed";
+import {
+  createAdminRoleAndUser,
+  createNormalRoleAndUser,
+} from "../prisma/seeds/seed";
+
+// clean up database before everything.
+// This is related to the global teardown not being called if runing tests in UI mode.
+// Github issue: https://github.com/microsoft/playwright/issues/22008
+// Is closed but still not working.
+setup.beforeAll(async () => {
+  await truncateDb();
+});
 
 setup("let the admin user login with email and password", async ({ page }) => {
-  // clean up database before everything.
-  // This is related to the global teardown not being called if runing tests in UI mode.
-  // Github issue: https://github.com/microsoft/playwright/issues/22008
-  // Is closed but still not working.
-  await truncateDb();
-
   // arrange
   await createAdminRoleAndUser();
   await page.goto(adminUser.loginUrl);
@@ -21,11 +27,31 @@ setup("let the admin user login with email and password", async ({ page }) => {
   await page.getByRole("button", { name: /Sign in with credentials/i }).click();
 
   // assert
-  await expect(page).toHaveURL("/");
+  await expect(page).toHaveURL(adminUser.appUrl);
   await expect(
     page.getByRole("img", { name: "user profile avatar" }),
   ).toBeVisible();
   await expect(page.getByRole("button", { name: /Login/i })).not.toBeVisible();
 
   await page.context().storageState({ path: adminUser.storageSessionPath });
+});
+
+setup("let a normal user login with email and password", async ({ page }) => {
+  // arrange
+  await createNormalRoleAndUser();
+  await page.goto(normalUser.loginUrl);
+
+  // act
+  await page.getByLabel(/Email/i).fill(normalUser.email);
+  await page.getByLabel(/Password/i).fill(normalUser.plainPassword);
+  await page.getByRole("button", { name: /Sign in with credentials/i }).click();
+
+  // assert
+  await expect(page).toHaveURL(normalUser.appUrl);
+  await expect(
+    page.getByRole("img", { name: "user profile avatar" }),
+  ).toBeVisible();
+  await expect(page.getByRole("button", { name: /Login/i })).not.toBeVisible();
+
+  await page.context().storageState({ path: normalUser.storageSessionPath });
 });
