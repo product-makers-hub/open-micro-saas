512d846700629977d7590b71f5583d7c0983f7a1
Juan Correa <juan.correa.herrera@gmail.com>
2024-01-11 13:27:36 -0600

    feat: add redirect from login page to dashboard when the user is authenticated

---
+++ src/app/auth/login/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+import { authOptions } from "@/libs/auth/auth-options";
+import { authConfig } from "@/config";
+
+export default async function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect(authConfig.normalUserCallbackUrl);
+  }
+
+  return <>{children}</>;
+}
---
+++ tests/authenticated/auth/login-redirect.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+import { authConfig } from "@/config";
+
+test.describe("Login page authenticated user", () => {
+  test("should redirect to the admin user callback url page when the admin is authenticated and navigates to the login page", async ({
+    page,
+  }) => {
+    await page.goto(authConfig.loginUrl);
+
+    await expect(page).toHaveURL(authConfig.adminUserCallbackUrl);
+    await expect(page).not.toHaveURL(authConfig.loginUrl);
+  });
+});
