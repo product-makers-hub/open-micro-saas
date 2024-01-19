173d5625692d97610e8845ba36792d1d0d2c86f3
Juan Correa <juan.correa.herrera@gmail.com>
2024-01-19 12:10:58 -0600

    fix: remove shopping cart icon from navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,39 +17,6 @@ export const Navbar = () => {
         </Link>
       </div>
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-circle btn-ghost">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />
-              </svg>
-              <span className="badge indicator-item badge-sm">0</span>
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            className="card dropdown-content card-compact z-[1] mt-3 w-52 bg-base-100 shadow"
-          >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
-              </div>
-            </div>
-          </div>
-        </div>
         <div>
           {status === "unauthenticated" && (
             <Link href="/auth/login" className="btn">
