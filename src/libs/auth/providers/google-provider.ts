8e2729a102b16840722158bf4af86a268b19fd08
Juan Correa <juan.correa.herrera@gmail.com>
2024-03-25 14:44:36 -0600

    refactor:  GoogleProvider profile function

--- src/libs/auth/providers/google-provider.ts
+++ src/libs/auth/providers/google-provider.ts
@@ -1,22 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 
-import { getUserByEmail } from "@/repositories/user-repository";
-
 export const getGoogleProvider = () =>
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    async profile(profile) {
-      const user = await getUserByEmail(profile.email);
-
-      return {
-        id: profile.sub,
-        name: profile.given_name ? profile.given_name : profile.name,
-        email: profile.email,
-        image: profile.picture,
-        createdAt: new Date(),
-        isActive: user?.isActive || true,
-        role: user?.role || "USER",
-      };
-    },
   });
