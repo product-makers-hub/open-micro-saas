219e16737d41184decd99d848c12500df8c606f3
Juan Correa <juan.correa.herrera@gmail.com>
2024-04-05 12:09:13 -0600

    feat: add initial middleware for request handling

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log("Middleware: ", request.nextUrl.pathname);
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
