c4040438ec51ff923751bd88953c7dea7c97759d
Juan Correa <juan.correa.herrera@gmail.com>
2024-04-19 15:09:21 -0600

    feat: add user sidenav component and use it to the user dashboard

---
+++ src/app/(user)/_components/user-sidenav.tsx
@@ -0,0 +1,23 @@
+import { authenticatedUserNavLinks } from "@/config/navigation-links-config";
+import { Button } from "@/components/ui/button";
+import { Link } from "@/components/ui/link";
+
+export const UserSidenav = () => {
+  return (
+    <aside
+      className="hidden max-h-full lg:block w-56 flex-shrink-0 border-r-2"
+      role="navigation"
+      aria-label="User sidenav"
+    >
+      <ul className="">
+        {authenticatedUserNavLinks.map((link) => (
+          <Button asChild key={link.url} variant="link">
+            <Link className="mt-4" href={link.url}>
+              {link.title}
+            </Link>
+          </Button>
+        ))}
+      </ul>
+    </aside>
+  );
+};
--- tests/authenticated/normal-user/dashboard.spec.ts
+++ tests/authenticated/normal-user/dashboard.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 
+import { authConfig } from "@/config/auth-config";
 import { normalUser } from "../../data/normal-user";
 
 test.use({
@@ -8,21 +9,39 @@ test.use({
 
 test.describe("Active user dashboard", () => {
   test("should display the page header title", async ({ page }) => {
-    await page.goto("/dashboard");
+    await page.goto(authConfig.normalUserCallbackUrl);
     await expect(
       page.getByRole("heading", { name: /dashboard/i }),
     ).toBeVisible();
   });
 
   test("should display the user's name", async ({ page }) => {
-    await page.goto("/dashboard");
+    await page.goto(authConfig.normalUserCallbackUrl);
     await expect(page.getByText(normalUser.name)).toBeVisible();
   });
 
   test("should display the subscription status", async ({ page }) => {
-    await page.goto("/dashboard");
+    await page.goto(authConfig.normalUserCallbackUrl);
     await expect(
       page.getByRole("heading", { name: /Thank you for subscribing/i }),
+    ).toBeVisible();
+  });
+
+  test("user dashboard has a sidenav navigation", async ({ page }) => {
+    await page.goto(authConfig.normalUserCallbackUrl);
+
+    await expect(
+      page.getByRole("navigation", { name: /user sidenav/i }),
+    ).toBeVisible();
+  });
+
+  test("user dashboard sidebav has the navigation links", async ({ page }) => {
+    await page.goto(authConfig.normalUserCallbackUrl);
+
+    await expect(
+      page
+        .getByRole("navigation", { name: /user sidenav/i })
+        .getByRole("link", { name: /dashboard/i }),
     ).toBeVisible();
   });
 });
